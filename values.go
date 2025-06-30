55ea5cd13f02a12e88c63af4a769420fb643e52c
felver-dev <felver.ngouala@gmail.com>
2025-06-30 16:33:38 +0000

    values:done

---
+++ values.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func printValues() {
+	var name = "jules"
+	var age = 25
+	var isEmployed = true
+	var height = 1.75
+	var salary = 50000.50
+
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Employed:", isEmployed)
+	fmt.Println("Height:", height)
+	fmt.Println("Salary:", salary)
+}
